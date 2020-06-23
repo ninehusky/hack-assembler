398e10dc53601fb1af6cc87fc10c9bf9521a1168
Andrew <acheung8@uw.edu>
2020-06-23 15:47:46 -0700

    Added Code module and Unit Tests

---
+++ hack-assembler/src/main/java/org/nineradio/Code.java
@@ -0,0 +1,92 @@
+/**
+ * Translates Hack assembly language mnemonics into binary codes.
+ */
+
+package org.nineradio;
+
+import java.util.*;
+import java.io.*;
+
+public class Code {
+    private static final String SYMBOL_BASE_URL = "-symbols.csv";
+    private Map<String, String> destMap;
+    private Map<String, String> compMap;
+    private Map<String, String> jumpMap;
+
+    /**
+     * Constructs a new Code.
+     */
+    public Code() {
+        destMap = new HashMap<>();
+        compMap = new HashMap<>();
+        jumpMap = new HashMap<>();
+        populateMaps("dest", destMap);
+        populateMaps("comp", compMap);
+        populateMaps("jump", jumpMap);
+    }
+
+    /**
+     * Returns the binary code of the dest mnemonic.
+     * @param mnemonic - the dest mnemonic
+     * @return binary equivalent of the mnemonic
+     */
+    public String dest(String mnemonic) {
+        return getSymbol(mnemonic, "000", destMap);
+    }
+
+    /**
+     * Returns the binary code of the comp mnemonic.
+     * @param mnemonic - the comp mnemonic
+     * @return binary equivalent of the mnemonic
+     */
+    public String comp(String mnemonic) {
+        return getSymbol(mnemonic, "invalid", compMap);
+    }
+
+    /**
+     * Returns the binary code of the jump mnemonic.
+     * @param mnemonic - the jump mnemonic
+     * @return binary equivalent of the mnemonic
+     */
+    public String jump(String mnemonic) {
+        return getSymbol(mnemonic, "000", jumpMap);
+    }
+
+    /**
+     * Returns the binary equivalent of the given mnemonic, or minimum if the
+     * mnemonic is empty.
+     * @param mnemonic - the Hack machine language to translate
+     * @param minimum - the String to return if mnemonic is empty
+     * @param map - the mnemonic/binary table for the command
+     * @return binary equivalent of mnemonic
+     */
+    private String getSymbol(String mnemonic, String minimum, Map<String, String> map) {
+        if (mnemonic.isEmpty()) {
+            return minimum;
+        }
+        String equivalent = map.get(mnemonic);
+        if (equivalent == null) {
+            throw new IllegalStateException("Could not locate binary equivalent of " + mnemonic);
+        }
+        return equivalent;
+    }
+
+    /**
+     * Populates the comp, dest, and jump maps to be used in the translation.
+     * @param root - type of command to load map for
+     * @param map - map to populate
+     */
+    private void populateMaps(String root, Map<String, String> map) {
+        try {
+            InputStream is = getClass().getClassLoader().getResourceAsStream(root + SYMBOL_BASE_URL);
+            Scanner input = new Scanner(is);
+            while (input.hasNextLine()) {
+                String[] keyValuePair = input.nextLine().split(",");
+                map.put(keyValuePair[0], keyValuePair[1]);
+            }
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ hack-assembler/src/test/java/org/nineradio/CodeTest.java
@@ -0,0 +1,73 @@
+package org.nineradio;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+
+import java.util.*;
+
+public class CodeTest {
+    private Code c;
+
+    @Before
+    public void setupTest() {
+        c = new Code();
+    }
+
+    
+    @Test
+    public void constructorPasses() {
+        c = new Code();
+    }
+
+    @Test
+    public void destMnemonicsReturnCorrectValues() {
+        // I am so sorry
+        List<String> destMnemonics = Arrays.asList("", "M", "D", "MD", "A", "AM", "AD", "AMD");
+        List<String> binary = Arrays.asList("000", "001", "010", "011", "100", "101", "110", "111");
+        List<String> binaryTest = new ArrayList<>();
+
+        for (String mnemonic : destMnemonics) {
+            binaryTest.add(c.dest(mnemonic));
+        }
+
+        assertEquals(binaryTest, binary);
+
+    }
+
+    @Test
+    public void compMnemonicsReturnCorrectValues() {
+        List<String> compMnemonics = Arrays.asList("0", "1", "-1", "D", "A", "M", "!D", "!A", "!M",
+                                                   "-D", "-A", "-M", "D+1", "A+1", "M+1", "D-1",
+                                                   "A-1", "M-1", "D+A", "D+M", "D-A", "D-M", "A-D",
+                                                   "M-D", "D&A", "D&M", "D|A", "D|M");
+        List<String> binary = Arrays.asList("101010", "111111", "111010", "001100", "110000",
+                                            "110000", "001101", "110001", "110001", "001111", "110011",
+                                            "110011", "011111", "110111", "110111", "001110",
+                                            "110010", "110010", "000010", "000010", "010011", "010011",
+                                            "000111", "000111", "000000", "000000", "010101", "010101");
+
+        List<String> binaryTest = new ArrayList<>();
+
+        assertEquals(compMnemonics.size(), binary.size());
+        for (String mnemonic : compMnemonics) {
+            binaryTest.add(c.comp(mnemonic));
+        }
+
+        assertEquals(binaryTest, binary);
+    }
+
+    @Test
+    public void jumpWorks() {
+        List<String> jumpMnemonics = Arrays.asList("", "JGT", "JEQ", "JGE", "JLT", "JNE",
+                                                   "JLE", "JMP");
+        List<String> binary = Arrays.asList("000", "001", "010", "011", "100", "101", "110", "111");
+        List<String> binaryTest = new ArrayList<>();
+
+        for (String mnemonic : jumpMnemonics) {
+            binaryTest.add(c.jump(mnemonic));
+        }
+
+        assertEquals(binaryTest, binary);
+    }
+}
